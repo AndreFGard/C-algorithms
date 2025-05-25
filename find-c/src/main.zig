a5072f730297b8e44d8f41380f31f55c6546688b
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2025-05-25 10:28:40 -0300

    a

---
+++ find-c/src/main.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+
+const c = @cImport({
+    @cInclude("unistd.h");
+});
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+
+    const argv = std.os.argv;
+    const argc: c_int = @intCast(argv.len);
+    var opt: c_int = c.getopt(argc, argv.ptr, "r:");
+    const optind = &c.optind;
+
+    const optarg =;
+    const defaultdir = "./";
+    while (opt != -1) {
+        switch (opt) {
+            'r' => {
+                //const optarg = @as([*:0]u8, c.optarg);
+                std.debug.print("Using format: {s}\n", .{optarg});
+            },
+            '?' => {
+                std.debug.print("unknown option: {c}\n", .{@as(u8, @intCast(c.optopt))});
+            },
+            else => unreachable,
+        }
+        opt = c.getopt(argc, argv.ptr, "r:");
+    }
+    const basedir = if (optind.* < argc)
+        std.mem.span(argv[@intCast(optind.*)])
+    else
+        defaultdir;
+    std.debug.print("your starting directory is argument {s}\n", .{@as([*:0]u8, @constCast(basedir))});
+}
