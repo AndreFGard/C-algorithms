83db35b2a69a85db52432929ceffc019ca9970d9
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2025-05-25 10:41:16 -0300

    a

--- find-c/src/main.zig
+++ find-c/src/main.zig
@@ -7,21 +7,21 @@ const c = @cImport({
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
-    const allocator = arena.allocator();
-
+    //const allocator = arena.allocator();
 
     const argv = std.os.argv;
     const argc: c_int = @intCast(argv.len);
     var opt: c_int = c.getopt(argc, argv.ptr, "r:");
     const optind = &c.optind;
 
-    const optarg =;
+    var filter: [*c]const u8 = "";
     const defaultdir = "./";
     while (opt != -1) {
         switch (opt) {
             'r' => {
+                filter = @as([*:0]u8, c.optarg);
                 //const optarg = @as([*:0]u8, c.optarg);
-                std.debug.print("Using format: {s}\n", .{optarg});
+                std.debug.print("Using format: {s}\n", .{filter});
             },
             '?' => {
                 std.debug.print("unknown option: {c}\n", .{@as(u8, @intCast(c.optopt))});
